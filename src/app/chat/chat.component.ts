1e5fdfcda04009d876548ee376b1b42210fa4d98
Brenton Klassen <Brenton.Klassen@vml.com>
2018-07-21 16:25:55 -0500

    add Twilio client

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+declare global {
+  interface Window { Twilio: any; }
+}
 
 @Component({
   selector: 'app-chat',
@@ -7,7 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: Http) {
+    const url = 'http://localhost:3000/token/brenton?device=browser'
+    http.get(url).subscribe((res:Response) => {
+      const token = res.json().token;
+      window.Twilio.Chat.Client.create(token).then(client => {
+        console.log('bk client', client);
+      });
+    });
+  }
 
   ngOnInit() {
   }
