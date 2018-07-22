de27ff4597f518d4142b03587be611bc4c56d426
Brenton Klassen <Brenton.Klassen@vml.com>
2018-07-22 03:02:22 -0500

    remove duplicate import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { ApiService } from './api.service';
 import { TokenService } from './token.service';
 import { Component } from '@angular/core';
-import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
