f7bbcd97c37047443ea08bb05f2599629467f1e6
Parth Mody <parth.mody@vml.com>
2018-07-21 23:44:14 -0500

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,39 @@
+import { HomeComponent } from './home/home.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
 import { HttpModule } from '../../node_modules/@angular/http';
 import { CreateTribeComponent } from './create-tribe/create-tribe.component';
+import { Routes } from '@angular/router';
 
+const appRoutes: Routes = [
+  { path: 'start-tribe', component: CreateTribeComponent },
+  { path: 'chat', component: ChatComponent },
+  { path: '', component: HomeComponent}
+
+  /* { path: 'crisis-center', component: CrisisListComponent } */
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     ChatComponent,
-    CreateTribeComponent
+    CreateTribeComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [HttpModule],
   bootstrap: [AppComponent]
